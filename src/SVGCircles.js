4c62f65cdf855eb7372cbe109a2f697057ee8364
Colin <colinxsmith@gmail.com>
2024-07-04 23:34:51 +0100

    .

--- src/SVGCircles.js
+++ src/SVGCircles.js
@@ -1,14 +1,16 @@
 import React from "react"
 import * as d3 from "d3";
 export const SVGCircles = ({ data, diameter, fill ,clicker}) => {
-    const changer=(ii)=>{
+    const changer=(ii,back=0)=>{
       console.log("Index is "+ii);
       const pp=d3.select("svg#svg1").selectAll("circle").nodes()[ii];
-      d3.select(pp).attr("fill","red");
+      const orig=ii%2===0?"cyan":"lightgreen";
+      if(back===1)d3.select(pp).attr("fill","red");
+      else d3.select(pp).attr("fill",orig);
       console.log(d3.select(pp).attr("fill"));
     }
     const toDisc = (i) => (
-      <circle ind={i} r={i%2===1?diameter / 2:diameter/3} cx={i * diameter} cy={diameter} fill={i%2===0?"cyan":"lightgreen"} onClick={()=>changer(i)} />
+      <circle ind={i} r={i%2===1?diameter / 2:diameter/3} cx={i * diameter} cy={diameter} fill={i%2===0?"cyan":"lightgreen"} onPointerOver={()=>changer(i,1)} onPointerLeave={()=>changer(i)} />
     );
   return (
       <div>
